9738f339e2852e6f9f41306e5d0b8f9a51f59368
khembo <khembo.douglas.juma@gmail.com>
2025-06-02 12:36:25 +0300

    main urls

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # <-- add this line to route API endpoints
+    path('api/', include('chats.urls')),           # include chats app urls at /api/
+    path('api-auth/', include('rest_framework.urls')),  # enable login/logout views for browsable API
 ]
