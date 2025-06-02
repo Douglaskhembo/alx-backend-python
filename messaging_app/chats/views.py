d09202307a24902dac5d429969c736a56a1ef4b8
khembo <khembo.douglas.juma@gmail.com>
2025-06-02 12:24:28 +0300

    task 3

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+
+from .models import Conversation, Message, User
+from .serializers import ConversationSerializer, MessageSerializer, UserSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Return conversations where user is participant
+        return Conversation.objects.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def add_participant(self, request, pk=None):
+        conversation = self.get_object()
+        user_id = request.data.get('user_id')
+        try:
+            user = User.objects.get(user_id=user_id)
+            conversation.participants.add(user)
+            return Response({'status': 'participant added'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter messages by conversations user is participant in
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation_id = self.request.data.get('conversation')
+        conversation = get_object_or_404(Conversation, conversation_id=conversation_id)
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied("You are not a participant of this conversation.")
+        serializer.save(sender=self.request.user, conversation=conversation)
