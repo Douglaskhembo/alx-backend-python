4707a81b8190bbde379e583c3799da43780f9c4f
khembo <khembo.douglas.juma@gmail.com>
2025-06-02 12:08:33 +0300

    serializers modification

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,19 +1,31 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import User, Conversation, Message
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'full_name']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField()
 
     class Meta:
         model = Message
         fields = ['message_id', 'conversation', 'sender', 'message_body', 'sent_at']
         read_only_fields = ['message_id', 'sent_at', 'sender']
+
+    def validate_message_body(self, value):
+        if not value.strip():
+            raise ValidationError("Message body cannot be empty or whitespace.")
+        return value
 
 
 class ConversationSerializer(serializers.ModelSerializer):
