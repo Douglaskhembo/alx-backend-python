399be042758e06e45027a84d6991db8d74ac3940
khembo <khembo.douglas.juma@gmail.com>
2025-06-06 11:43:56 +0300

    Add Permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,7 +1,26 @@
-from rest_framework import permissions  # <-- This is required
+from rest_framework import permissions
+from .models import Conversation, Message
 
-from rest_framework.permissions import BasePermission
 
-class IsParticipant(BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Custom permission to allow access only to participants of a conversation.
+    """
+
+    def has_permission(self, request, view):
+        # Ensure the user is authenticated
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
+        user = request.user
+
+        # For Conversation objects
+        if isinstance(obj, Conversation):
+            return user in obj.participants.all()
+
+        # For Message objects
+        if isinstance(obj, Message):
+            if request.method in ['GET', 'PUT', 'PATCH', 'DELETE']:
+                return user in obj.conversation.participants.all()
+
+        return False
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,23 +1,24 @@
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.status import HTTP_403_FORBIDDEN
 from django.shortcuts import get_object_or_404
 
 from .models import Conversation, Message, User
 from .serializers import ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsParticipantOfConversation]
     filter_backends = [filters.SearchFilter]
     search_fields = ['participants__username']
 
     def get_queryset(self):
-        # Return conversations where the user is a participant
+        # Return only conversations where the user is a participant
         return Conversation.objects.filter(participants=self.request.user)
 
     def perform_create(self, serializer):
@@ -38,12 +39,11 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsParticipantOfConversation]
     filter_backends = [filters.SearchFilter]
     search_fields = ['message_body']
 
     def get_queryset(self):
-        # Restrict messages to the specific conversation if user is a participant
         conversation_pk = self.kwargs['conversation_pk']
         conversation = get_object_or_404(
             Conversation,
@@ -53,11 +53,13 @@ class MessageViewSet(viewsets.ModelViewSet):
         return Message.objects.filter(conversation=conversation)
 
     def perform_create(self, serializer):
-        # Use conversation from the nested route and check participant access
         conversation_pk = self.kwargs['conversation_pk']
-        conversation = get_object_or_404(
-            Conversation,
-            conversation_id=conversation_pk,
-            participants=self.request.user
-        )
+        conversation = get_object_or_404(Conversation, conversation_id=conversation_pk)
+
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied(
+                detail="You are not a participant of this conversation.",
+                code=HTTP_403_FORBIDDEN
+            )
+
         serializer.save(sender=self.request.user, conversation=conversation)
