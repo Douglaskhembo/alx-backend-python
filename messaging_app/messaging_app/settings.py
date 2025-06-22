79a0ab5a16a13ba2d62746208e53eeb2cce41864
khembo <khembo.douglas.juma@gmail.com>
2025-06-22 15:19:13 +0300

    refactor

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -54,14 +54,15 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DB'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
